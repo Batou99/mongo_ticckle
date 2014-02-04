0c6fa0c76b4d7e4f6f741718dc10f11549cd2b6d
Artur Hebda <arturhebda@gmail.com>
2014-02-04 11:49:40 +0100

    Get rid of share configs in favour of configurations.

--- models/user.rb
+++ models/user.rb
@@ -7,6 +7,12 @@ require File.join(File.dirname(__FILE__), 'device')
 class User < ActiveRecord::Base
   # FIXME: Move to yml file
   ONLINE_PRESENCE_TIMEOUT = 6.hours
+  CONFIG_MAPPING = {
+    email_ticckle_notification: :ticckle_notifications,
+    email_video_response_notification: :reply_notifications,
+    email_weekly_notification: :digest_emails,
+    email_debate_contribution_notification: :join_notifications
+  }
 
   has_many :videos
   has_many :topics, through: :videos
@@ -36,5 +42,29 @@ class User < ActiveRecord::Base
   def get_share_config(key)
     out = share_configs.where(key: key).first.value rescue false
     out == "1" || out == "on"
+  end
+
+  # - name - name of configuration from Configuration (not share config)
+  def get_config(name)
+    share_config = share_configs.where(key: CONFIG_MAPPING.key(name)).first
+    get_share_config  = -> { share_config.value if share_config }
+    get_configuration = -> { configuration.public_send(name) if configuration }
+
+    share_config_defined = !get_share_config.call.nil?
+    configuration_defined = !get_configuration.call.nil?
+
+    truthy_values = ["1", "on", true, nil]
+
+    config = if share_config_defined && configuration_defined
+      get_configuration
+    elsif share_config_defined
+      get_share_config
+    elsif configuration_defined
+      get_configuration
+    else
+      -> { nil }
+    end
+
+    truthy_values.include?(config.call)
   end
 end
